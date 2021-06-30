eb9464fe856f68d664ee823daa31c866544faf74
Fayfay Ning <fayfayning@Fayfays-MacBook-Pro.local>
2021-06-29 23:56:39 -0400

    minor updates

--- exhaustive.py
+++ exhaustive.py
@@ -32,20 +32,12 @@ def WHM(G, H):
     edges = get_edges(H)
     #print(edges)
     tree_len = len(H)
-    perms = list(itertools.permutations(range(1, n + 1), tree_len))
-    generator_expression = (i for i in itertools.permutations(range(1, n + 1), tree_len))
-
-    # for i in generator_expression:
-    #     print(i, end=" ")
-
-    try:
-        ncpus = int(os.environ['SLURM_JOB_CPUS_PER_NODE'])
-    except KeyError:
-        ncpus = mp.cpu_count()
-    pool = mp.Pool(ncpus)
-    checks = pool.starmap(map_fetch, [(map, G, edges) for map in generator_expression])
-    fin = sum(checks) / aut(H)
-    return fin
+    perms = itertools.permutations(range(1, n + 1), tree_len)
+    sum = 0
+    for map in perms:
+        sum += map_fetch(map, G, edges)
+    sum = sum / aut(H)
+    return sum
 
 def fAB(A, B, K):
     T = generateFreeTrees(K)
@@ -123,22 +115,21 @@ def test():
          [0, 1, 0, 0, 1],
          [1, 1, 0, 1, 0]]
     M = np.array(M)
-    H = [0, 1, 1, 1]
+    H = [0, 1, 2, 1, 1]
     print(WHM(M, H))
 
     K = len(H) - 1
     print(fAB(M, M, K))
 
     # C = rand_assign(K, M.shape[1])
-    C = [1, 2, 1, 2, 2]
+    C = [1, 2, 3, 2, 2]
     print(C)
     print(algorithmOne(H, M, C))
 
 def timing():
-    n_lst = [i * 5 for i in range(1, 10)]
+    n_lst = [i * 10 for i in range(1, 11)]
     for n in n_lst:
-        H = [0]
-        H.extend([1 for i in range(n - 1)])
+        H = [0, 1, 1]
         G = erd_ren(n, 0.1)
         start = time.time()
         WHM(G, H)
@@ -147,6 +138,8 @@ def timing():
 if __name__ == '__main__':
 
     test()
+    #timing()
+    #exh_sim2(1, 20, 0.1, 1, 4)
 
     """
     # m, n, p, s, K
@@ -181,4 +174,35 @@ WMH = 6
 H = [0, 1, 1, 1]
 C = [1, 2, 3, 3, 4]
 should be 12
+"""
+
+"""
+def map_fetch(map, G, edges):
+    check = 1
+    for edge in edges:
+        # print(map)
+        m1 = map[edge[0] - 1] - 1
+        m2 = map[edge[1] - 1] - 1
+        # print(edge, m1, m2)
+        check *= G[m1][m2]
+    return check
+    
+def WHM(G, H):
+    n = G.shape[1]
+    edges = get_edges(H)
+    #print(edges)
+    tree_len = len(H)
+    perms = (i for i in itertools.permutations(range(1, n + 1),tree_len))
+
+    # for i in generator_expression:
+    #     print(i, end=" ")
+
+    try:
+        ncpus = int(os.environ['SLURM_JOB_CPUS_PER_NODE'])
+    except KeyError:
+        ncpus = mp.cpu_count()
+    pool = mp.Pool(ncpus)
+    checks = pool.starmap(map_fetch, [(map, G, edges) for map in perms])
+    fin = sum(checks) / aut(H)
+    return fin
 """
