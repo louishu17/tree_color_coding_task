ff469f72b2419e424c242b777ab02254a2ac9f04
Fayfay Ning <fayfayning@rocketmail.com>
2021-08-05 18:07:43 -0400

    facebook data cleaning

--- facebook.py
+++ facebook.py
@@ -17,7 +17,53 @@ from scipy import io
 import os
 from os import listdir
 from itertools import combinations_with_replacement
+import itertools
 import re
+
+def simi_spec_combo_old(directory, dict, combo):
+    path1 = "{}/{}".format(directory, combo[0])
+    file1 = open(path1, 'r')
+    read_1 = file1.readlines()
+    file1.close()
+
+    path2 = "{}/{}".format(directory, combo[1])
+    file2 = open(path2, 'r')
+    read_2 = file2.readlines()
+    file2.close()
+
+    lines1 = len(read_1)
+    lines2 = len(read_2)
+    line_num = min(lines1, lines2)
+
+    df_part = pd.DataFrame()
+
+    for i in range(1, line_num):
+        line1 = read_1[i].split(',')
+        line2 = read_2[i].split(',')
+        vals = [float(line1[j]) * float(line2[j]) for j in range(len(line1))]
+        val = sum(vals)
+        new_row = {'File A': dict[combo[0]], 'File B': dict[combo[1]],
+                   'n': int(i + 2), 'Similarity': val}
+        df_part = df_part.append(new_row, ignore_index = True)
+    return df_part
+
+def counts_to_similarity_old(directory):
+    df = pd.DataFrame()
+    dict = {}
+    file_lst = []
+    for file in listdir(directory):
+        if file != "gitkeep.txt" and ".txt" in file:
+            file_lst.append(file)
+            dict[file] = re.split('[_.]',file)[1]
+    combos = combinations_with_replacement(file_lst, 2)
+    #print(dict)
+    #print(list(combos))
+    for i in combos:
+        #print(i)
+        combo_data = simi_spec_combo_old(directory, dict, i)
+        df = df.append(combo_data)
+    #df.to_csv('Similarity_Unnormalized.txt', encoding='utf-8', index=False)
+    return df
 
 def sample_n(A, B, n, p):
     x = random.randint(0, len(A) - n - 1)
@@ -206,6 +252,8 @@ def one_per_array(directory):
             int += 1
 
 def simi_spec_combo(directory, dict, combo):
+    aut = [1, 2, 3, 6, 11, 23, 47, 106]
+
     path1 = "{}/{}".format(directory, combo[0])
     file1 = open(path1, 'r')
     read_1 = file1.readlines()
@@ -216,20 +264,55 @@ def simi_spec_combo(directory, dict, combo):
     read_2 = file2.readlines()
     file2.close()
 
-    lines1 = len(read_1)
-    lines2 = len(read_2)
-    line_num = min(lines1, lines2)
+    lines1_num = len(read_1)
+    lines2_num = len(read_2)
+    lines_num = min(lines1_num, lines2_num)
 
     df_part = pd.DataFrame()
 
-    for i in range(1, line_num):
-        line1 = read_1[i].split(',')
-        line2 = read_2[i].split(',')
-        vals = [float(line1[j]) * float(line2[j]) for j in range(len(line1))]
-        val = sum(vals)
+    global incomplete
+
+    for i in range(1, lines_num, 2):
+        n_num = int(i / 2 + 3)
+
+        ##
+        n_num = 5
+        ##
+
+        line1 = read_1[i].split("||")
+        line2 = read_2[i].split("||")
+        it_num = min(len(line1), len(line2))
+
+        if len(line1) != 1000:
+            tup1 = tuple([dict[combo[0]], len(line1)])
+            if tup1 not in incomplete:
+                incomplete.append(tup1)
+        if len(line2) != 1000:
+            tup2 = tuple([dict[combo[1]], len(line2)])
+            if tup2 not in incomplete:
+                incomplete.append(tup2)
+
+        avg = 0
+        for j in range(0, it_num):
+            m_tre1 = line1[j].split(",")
+            m_tre2 = line2[j].split(",")
+            if m_tre1[-1] == '':
+                del m_tre1[-1]
+            if m_tre2[-1] == '':
+                del m_tre2[-1]
+            if (len(m_tre1) != aut[n_num - 3] or len(m_tre2) != aut[n_num - 3]):
+                it_num = it_num - 1
+                break
+            else:
+                vals = [float(m_tre1[k]) * float(m_tre2[k]) for k in range(aut[
+                    n_num - 3])]
+                val = sum(vals)
+                avg += val
+        avg = avg / it_num
         new_row = {'File A': dict[combo[0]], 'File B': dict[combo[1]],
-                   'n': int(i + 2), 'Similarity': val}
+                       'n': n_num, 'Similarity': avg, 'Iterations': it_num}
         df_part = df_part.append(new_row, ignore_index = True)
+
     return df_part
 
 
@@ -248,7 +331,6 @@ def counts_to_similarity(directory):
         #print(i)
         combo_data = simi_spec_combo(directory, dict, i)
         df = df.append(combo_data)
-    print(df)
     #df.to_csv('Similarity_Unnormalized.txt', encoding='utf-8', index=False)
     return df
 
@@ -288,7 +370,6 @@ def normalize_df(df):
         j = 0
 
     #new_df.to_csv('Similarity_Normalized.txt', encoding='utf-8', index=False)
-    print(new_df)
     return new_df
 
 def analyze_err_time(directory):
@@ -298,6 +379,8 @@ def analyze_err_time(directory):
     dict_max = {}
     dict_err = {}
     unknown = []
+    memory_error = []
+    mem_names = []
     for file in listdir(directory):
         if '.out' in file:
             f = open(file, 'r')
@@ -325,6 +408,8 @@ def analyze_err_time(directory):
                 dict_err[name] = 'No Error'
             elif out_of_mem in err_lines[0]:
                 dict_err[name] = 'Out of Memory'
+                memory_error.append(tuple([name, num, len(timings) + 2]))
+                mem_names.append(name)
             else:
                 dict_err[name] = 'Unknown'
                 unknown.append(tuple([name, num]))
@@ -334,22 +419,140 @@ def analyze_err_time(directory):
     print(dict_num)
     print(dict_err)
     print(unknown)
+    print(memory_error)
+    print(mem_names)
 
     return [dict_timing, dict_max, dict_num, dict_err, unknown]
 
-def comp_err_out():
-    slurm_path_no = '/Users/fayfayning/Desktop/big_boi/slurms2/no'
+def comp_err_out(path):
     [no_timing, no_max, no_num, no_err, no_unk] = analyze_err_time(
-        slurm_path_no)
+        path)
 
-    slurm_path_o = '/Users/fayfayning/Desktop/big_boi/slurms2/o'
-    [o_timing, o_max, o_num, o_err, o_unk] = analyze_err_time(slurm_path_o)
+    #slurm_path_o = '/Users/fayfayning/Desktop/big_boi/slurms2/o'
+    #[o_timing, o_max, o_num, o_err, o_unk] = analyze_err_time(slurm_path_o)
 
-    for key in o_timing.keys():
-        print(key, 'no', no_timing[key])
-        print(key, 'o', o_timing[key])
-        print(key, 'no_err', no_err[key], no_max[key])
-        print(key, 'o_err', o_err[key], o_max[key])
+    for key in no_timing.keys():
+        #print(key, 'no', no_timing[key])
+        #print(key, 'o', o_timing[key])
+        #print(key, 'no_err', no_err[key], no_max[key])
+        #print(key, 'o_err', o_err[key], o_max[key])
+        pass
+
+def get_sizes(directory):
+
+    files_lst = []
+    nodes = []
+    edges = []
+    for file in listdir(directory):
+        if file != "gitkeep.txt" and ".txt" in file:
+            files_lst.append(file[:-4])
+            f = open(directory + '/' + file, 'r')
+            line1 = int(f.readline())
+            line2 = int(f.readline())
+            #file_sizes.append(os.path.getsize(directory + '/' + file))
+            nodes.append(line1)
+            edges.append(line2)
+    print("files_lst =", files_lst)
+    print("nodes =", nodes)
+    print("edges =", edges)
+    print(len(files_lst), len(nodes), len(edges))
+
+def get_big_files(directory, nodes, edges):
+
+    edge_density = edges / (nodes * (nodes - 1) / 2)
+    print(edge_density)
+
+    files_lst = []
+    for file in listdir(directory):
+        if file != "gitkeep.txt" and ".txt" in file:
+            f = open(directory + '/' + file, 'r')
+            temp_n = int(f.readline())
+            temp_e = int(f.readline())
+            temp_d = temp_e / (temp_n * (temp_n - 1) / 2)
+            if temp_n > nodes and temp_d > (edge_density * 1.1):
+                files_lst.append(file)
+                print(file)
+    print(len(files_lst))
+
+
+def export_combos(files_txt, num_arrays):
+    exp_dir = "samp_file_lists"
+
+    with open(files_txt) as f:
+        files_lst = [line.rstrip() for line in f]
+    if (files_lst[-1] == " ") or (files_lst[-1] == "\n"):
+        files_lst.pop(-1)
+
+    num_files = len(files_lst)
+    total_combos = (num_files * (num_files -1)) // 2 + num_files
+    print('total combos = ', total_combos)
+
+    combos = combinations_with_replacement(files_lst, 2)
+
+    comb_per_arr = total_combos // num_arrays
+    rem = total_combos % num_arrays
+    print(comb_per_arr)
+    print(rem)
+
+    for arr in range(1, num_arrays + 1):
+        lst = []
+        if arr <= rem:
+            it_slice = itertools.islice(combos, comb_per_arr + 1)
+        else:
+            it_slice = itertools.islice(combos, comb_per_arr)
+        for ele in it_slice:
+            lst.append(ele[0] + "," + ele[1] + "\n")
+        lst[-1] = lst[-1].strip()
+        file1 = open("{}/smp_lst_{}.txt".format(exp_dir, arr), "w")
+        file1.writelines(lst)
+        file1.close()
+
+def parse_samp_slurms(directory):
+    schools = []
+    compile_file = open("slurms_out.txt", "a")
+    for file in listdir(directory):
+        if ".out" in file:
+            file_op = open(directory + "/" + file)
+            line = file_op.readline()
+            line.strip()
+            entries = line.split(",")
+            real_line = entries[:3]
+            avg_entry = 0
+            for i in range(len(entries)):
+                if "avg:" in entries[i]:
+                    avg_entry = i
+            avg = entries[avg_entry].replace(" avg: ", "")
+            real_line.append(avg + "\n")
+            real_str = ",".join(real_line)
+            file_op.close()
+            compile_file.write(real_str)
+    compile_file.close()
+
+def samp_simi(directory):
+    df = pd.read_csv("slurms_out.txt", header=0, names=["File A", "File B", "n",
+                                              "Similarity"])
+    schools = []
+    self_df = {}
+    for index, row in df.iterrows():
+        if row["File A"] == row["File B"]:
+            tup = tuple([row['File A'], row['n']])
+            self_df[tup] = row['Similarity']
+            schools.append(row["File A"])
+    print(schools)
+    print(self_df)
+    file_lst = []
+    new_df = pd.DataFrame()
+    for index, row in df.iterrows():
+        if row["File A"] != row["File B"]:
+            tup_1 = tuple([row['File A'], row['n']])
+            tup_2 = tuple([row['File B'], row['n']])
+            new_row = row
+            new_row.loc['Similarity'] = row.loc['Similarity'] / math.sqrt(
+                self_df[tup_1]) / math.sqrt(self_df[tup_2])
+            new_df = new_df.append(new_row)
+    new_df.to_csv('slurms_out_normalized.txt', encoding='utf-8', index=False)
+    return df
+
 
 if __name__ == '__main__':
     small_fb_path = "/Users/fayfayning/Documents/GitHub/fasciaGraphSimilarity/small_fb"
@@ -357,10 +560,34 @@ if __name__ == '__main__':
 
     #one_per_array(small_fb_path)
 
-    #output_to_df("small_fb_full_n.txt")
+    #output_to_df("small_fb_files/small_fb_full_n.txt")
 
-    count_tests_path = "/Users/fayfayning/Desktop/count_trees_no"
-    df_og = counts_to_similarity(count_tests_path)
-    normalize_df(df_og)
+    incomplete = []
 
-    #comp_err_out()
+    count_tests_path = "/Users/fayfayning/Desktop/big_boi/count_trees_no_added"
+    #df_new = counts_to_similarity(count_tests_path)
+    #print(df_new)
+    #df_new_norm = normalize_df(df_new)
+    #print(df_new_norm)
+
+    #print(incomplete)
+
+    slurms_path = '/Users/fayfayning/Desktop/big_boi/slurms_new'
+    #comp_err_out(slurms_path)
+
+    #count_tests_path_old = "/Users/fayfayning/Desktop/count_tests_big_boi"
+    #df_old = counts_to_similarity_old(count_tests_path_old)
+    #print(df_old)
+
+    #get_sizes(small_fb_path)
+
+    samp_files_path = "/Users/fayfayning/Documents/GitHub/fasciaGraphSimilarity/samp_files.txt"
+    #export_combos(samp_files_path, 1830)
+
+    #get_file_list(small_fb_path)
+
+    get_big_files(small_fb_path, 2000, 10000)
+
+    samp_slurms_path = "/Users/fayfayning/Desktop/big_boi/slurms_copy"
+    #parse_samp_slurms(samp_slurms_path)
+    #samp_simi(samp_slurms_path)
